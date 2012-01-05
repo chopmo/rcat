c7fedaadaafeee4a4ca99f5fee796fd333d989ad
Jacob Poulsgaard Tjoernholm <chopmo@gmail.com>
2012-01-05 13:40:04 +0100

    simplify

--- lib/rcat/display.rb
+++ lib/rcat/display.rb
@@ -9,10 +9,6 @@ module RCat
     def print_line(line)
       print line
     end
-
-    def reset
-      # Do nothing
-    end
   end
 
   class ExtraNewlineSqueezer
@@ -28,10 +24,6 @@ module RCat
         @printer.print_line(line)
       end
       @prev_line_blank = blank_line?(line)
-    end
-
-    def reset
-      @printer.reset
     end
   end
 
@@ -52,44 +44,32 @@ module RCat
         @line_number += 1
       end
     end
-
-    def reset
-      @printer.reset
-      @line_number = 1
-    end
   end
   
   class Display
     def initialize(params)
-      line_numbering_style   = params[:line_numbering_style]
-      squeeze_extra_newlines = params[:squeeze_extra_newlines]
+      @line_numbering_style   = params[:line_numbering_style]
+      @squeeze_extra_newlines = params[:squeeze_extra_newlines]
+    end
 
-      @printer = LinePrinter.new
+    def create_printer
+      printer = LinePrinter.new
 
-      if line_numbering_style
-        @printer = LineNumberer.new(@printer, :mode => line_numbering_style)
+      if @line_numbering_style
+        printer = LineNumberer.new(printer, :mode => @line_numbering_style)
       end
 
-      if squeeze_extra_newlines
-        @printer = ExtraNewlineSqueezer.new(@printer)
+      if @squeeze_extra_newlines
+        printer = ExtraNewlineSqueezer.new(printer)
       end
+      printer
     end
 
     def render(data)
-      @printer.reset
+      printer = create_printer
       data.lines.each do |line|
-        @printer.print_line(line)
+        printer.print_line(line)
       end
-    end
-
-    attr_reader :line_numbering_style, :squeeze_extra_newlines, :line_number
-
-    def print_labeled_line(line, line_number)
-      print "#{line_number.to_s.rjust(6)}\t#{line}" 
-    end
-
-    def print_unlabeled_line(line)
-      print line
     end
   end
 
